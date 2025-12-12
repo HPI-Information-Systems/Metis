1c3d0a5adc80f6d6713ebc6fe7671f6efe8fc035
jb3rndt <berndtjannis@gmail.com>
2025-12-12 15:47:12 +0100

    rename metric files

--- metis/metric/__init__.py
+++ metis/metric/__init__.py
@@ -1,10 +1,10 @@
 from .completeness.completeness import Completeness
 from .consistency.consistency import Consistency
-from .consistency.consistency_RuleBasedHinrichs import ConsistencyRuleBasedHinrichs
-from .correctness.correctness_Heinrich import CorrectnessHeinrich
+from .consistency.consistency_ruleBasedHinrichs import ConsistencyRuleBasedHinrichs
+from .correctness.correctness_heinrich import CorrectnessHeinrich
 from .metric import Metric
 from .minimality.column_minimality_duplicateCount import (
     column_minimality_duplicateCount,
 )
-from .timeliness.timeliness_Heinrich import TimelinessHeinrich
+from .timeliness.timeliness_heinrich import TimelinessHeinrich
 from .validity.out_of_vocabulary import OutOfVocabulary
