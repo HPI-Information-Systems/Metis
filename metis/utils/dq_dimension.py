db949db01de2730290807d99389ef11da1089564
Tomic Riedel <tomicriedel07@gmail.com>
2026-05-11 13:09:07 +0200

    feat(metric): add Accuracy data quality dimension

--- metis/utils/dq_dimension.py
+++ metis/utils/dq_dimension.py
@@ -4,6 +4,7 @@ from enum import StrEnum
 class DQDimension(StrEnum):
     """Data Quality Dimensions Enum. Primarily used for labeling DQResults inside each metric implementation with the DQ Dimension they assess."""
 
+    ACCURACY = "Accuracy"
     CONSISTENCY = "Consistency"
     CORRECTNESS = "Correctness"
     COMPLETENESS = "Completeness"
